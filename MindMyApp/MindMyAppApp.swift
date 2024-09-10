e65f3f5370671171e0fd88e15b00e4446a52960c
Filipe Patricio <facpatricio@gmail.com>
2024-09-10 13:22:34 +0100

    Initial Commit

---
+++ MindMyApp/MindMyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindMyAppApp.swift
+//  MindMyApp
+//
+//  Created by Filipe Patricio on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MindMyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
